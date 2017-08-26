271c1a990d33b0d31a083f45a2e39b71c1081197
Temuge <temuge@yext.com>
2017-08-26 14:16:33 -0400

    try adding static url

--- webapps/root/core/urls.py
+++ webapps/root/core/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root', settings.STATIC_ROOT}),
     url(r'^$', views.index, name='index'),
     url(r'^submitRegistration/$', views.submitRegistration, name='submitRegistration'),
     url(r'^registration/$', views.registration, name='registration'),
