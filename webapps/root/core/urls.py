795a7c66475340a62722daee3bd2c84c9abf888c
Temuge <temuge@yext.com>
2017-08-26 14:21:30 -0400

    change url to use static

--- webapps/root/core/urls.py
+++ webapps/root/core/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root', settings.STATIC_ROOT}),
     url(r'^$', views.index, name='index'),
     url(r'^submitRegistration/$', views.submitRegistration, name='submitRegistration'),
     url(r'^registration/$', views.registration, name='registration'),
     url(r'^confirmEmail/(?P<externalId>\w+)/(?P<id>\w+)/$', views.confirmEmail, name='confirmEmail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
