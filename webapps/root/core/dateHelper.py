075422acebae71b55d2157ab2a723919a22df142
Temuge <temuge@yext.com>
2017-08-26 13:19:19 -0400

    more stuff

---
+++ webapps/root/core/dateHelper.py
@@ -0,0 +1,28 @@
+import datetime;
+
+MAX_TIME_TO_BE_SCHOOL = 10
+MAX_AGE = 100
+
+def getPossibleYearsToBorn():
+    now = datetime.datetime.now()
+    return range(now.year-MAX_AGE, now.year+1)
+
+def getPossibleYearsToGraduate():
+    now = datetime.datetime.now()
+    return range(now.year, now.year+MAX_TIME_TO_BE_SCHOOL+1)
+    
+def getMonthsList():
+    return range(1, 13)
+
+def getDaysList():
+    return range(1,32)
+
+def getDateTimeContext():
+    context = {}
+    context['possibleYearsToBorn']=getPossibleYearsToBorn()
+    context['possibleYearsToGraduate']=getPossibleYearsToGraduate()
+    context['months']=getMonthsList()
+    context['days']=getDaysList()
+
+    return context;
+
---
+++ webapps/root/core/emailUtils.py
@@ -0,0 +1,26 @@
+import requests
+from django.template.loader import render_to_string
+from django.core.urlresolvers import reverse
+
+MAILGUN_SECRET_KEY = 'key-5a12a0cc0bc377c7b610ee95415c078c'
+MAILGUN_DOMAIN_TEST = 'sandbox8b895169a282427d892eae1142f022fc.mailgun.org'
+WHITELISTED_EMAIL_MAP = {"temuge@mit.edu" : "temuge.ez@gmail.com"}
+
+def send_email_confirmation_mail(firstName, schoolEmailAddress, url):
+        to = WHITELISTED_EMAIL_MAP[schoolEmailAddress] if schoolEmailAddress in WHITELISTED_EMAIL_MAP else schoolEmailAddress
+	domain = MAILGUN_DOMAIN_TEST
+	fromEmail = "AMSA <mailgun@{0}>".format(domain)
+	subject = "Amsa Registration Email Confirmation"
+	context = {'firstName': firstName, 'url': url}
+	html = render_to_string('confirmEmailMailTemplate.html', context)
+	send_mailgun_email(domain, fromEmail, [to], subject, html)
+
+def send_mailgun_email(domain, fromEmail, tos, subject, html):
+        #TODO: handle ssl warning!!!
+        r = requests.post(
+                "https://api.mailgun.net/v3/{0}/messages".format(domain),
+                auth=("api", MAILGUN_SECRET_KEY),
+                data={"from": fromEmail,
+                	  "to": tos,
+                	  "subject": subject,
+                	  "html": html})
