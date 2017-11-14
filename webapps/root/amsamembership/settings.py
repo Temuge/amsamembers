a1180b77b6e593e5eafc514ba5a56f67eb656e3b
Temuge <temuge@yext.com>
2017-11-14 00:11:38 -0500

    add more styles to views

--- webapps/root/amsamembership/settings.py
+++ webapps/root/amsamembership/settings.py
@@ -135,3 +135,6 @@ USE_TZ = True
 
 STATIC_ROOT = '/var/www/static/'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
--- webapps/root/amsamembership/urls.py
+++ webapps/root/amsamembership/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+import settings
 
 urlpatterns = [
 	url(r'^register/', include('core.urls', namespace="core")),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG == True:
+    urlpatterns += staticfiles_urlpatterns()
